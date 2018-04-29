07b956a8d3d8968f8aeed8c1bb3d801b85c03e50
SM-Wistful <wickedvirtue@yahoo.com>
2018-04-29 08:57:41 -0400

    update with latest fixes

--- heal.js
+++ heal.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Automatic Bull Helmet & Heal Mod! DESTROY SERVERS EASILY with this!
+// @name         Moomoo.io Automatic Bull Helmet & AutoHeal Mod! DESTROY SERVERS EASILY with this!
 // @namespace    -
-// @version      4.0
+// @version      5.1
 // @description  Autoheal
 // @author       Cloudy#9558
 // @match        *://moomoo.io/*
@@ -10,13 +10,18 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/msgpack5/4.0.1/msgpack5.js
 // ==/UserScript==
 
+const autoHealSpeed = 150; //Bigger number = SLOWER autoheal; fastest is 0
 
 const START_SSWX = [4, 132, 164, 116, 121, 112, 101, 2, 164, 100, 97, 116, 97, 147, 161, 52, 1, 192, 167, 111, 112, 116, 105, 111, 110, 115, 129, 168, 99, 111, 109, 112, 114, 101, 115, 115, 195, 163, 110, 115, 112, 161, 47];
 const END_SSWX = [4, 132, 164, 116, 121, 112, 101, 2, 164, 100, 97, 116, 97, 147, 161, 52, 0, 192, 167, 111, 112, 116, 105, 111, 110, 115, 129, 168, 99, 111, 109, 112, 114, 101, 115, 115, 195, 163, 110, 115, 112, 161, 47];
 
-const autoHealSpeed = 150; //Bigger number = SLOWER autoheal; fastest is 0
+const APPLE = [4, 132, 164, 116, 121, 112, 101, 2, 164, 100, 97, 116, 97, 147, 161, 53, 0, 212, 0, 0, 167, 111, 112, 116, 105, 111, 110, 115, 129, 168, 99, 111, 109, 112, 114, 101, 115, 115, 195, 163, 110, 115, 112, 161, 47];
+const COOKIE = [4, 132, 164, 116, 121, 112, 101, 2, 164, 100, 97, 116, 97, 147, 161, 53, 1, 212, 0, 0, 167, 111, 112, 116, 105, 111, 110, 115, 129, 168, 99, 111, 109, 112, 114, 101, 115, 115, 195, 163, 110, 115, 112, 161, 47];
 
+var currentHat = 0;
+var IN_PROCESS = false;
 var justDied = false;
+var recentHealth = 100;
 var ws;
 var MYID;
 var hasApple = true;
@@ -36,7 +41,7 @@ function encodeSEND(json){
 
 function bullHelmet(status){
     var dataTemplate = {"data":[], "options":{"compress":false}, "nsp": "/", "type": 2};
-    dataTemplate["data"]= ["13", 0, status == "on" ? 7 : 0, 0];
+    dataTemplate["data"]= ["13", 0, status == "on" ? 7 : currentHat, 0];
     //console.log(dataTemplate["data"]);
     let encoded = encodeSEND(dataTemplate);
     return encoded;
@@ -56,26 +61,33 @@ WebSocket.prototype.send = function(m){
     if (x_arr_SSX.length === 43 && autobull){
          if (x_arr_SSX.every( (num, idx) => START_SSWX[idx]==num )){
              console.log("started swing");
+             IN_PROCESS = true;
              this.oldSend(bullHelmet("on"));
          } else if (x_arr_SSX.every( (num, idx) => END_SSWX[idx]==num ) ){
              console.log("ended swing");
              this.oldSend(bullHelmet("off"));
+             IN_PROCESS = false;
          }
     }
 
     this.oldSend(m);
-    let y = msgpack5().decode(x.slice(1, x.length)).data;
-    //console.log("SENT");
-    //console.log(y);
-    let realData = msgpack5().decode(x.slice(1, x.length));
-    //console.log(realData.data[0]);
-    if (realData.data[0]=="5"){
-        if (realData.data[1] == 0 || realData.data[1] == 1) foodInHand = !foodInHand;
+    /*let usageArray = Array.from(new Uint8Array(m));
+    if (usageArray.length == 45){
+        if (usageArray[16] == 0 || usageArray[16] == 1) foodInHand = false;
         console.log(`Food in hand: ${foodInHand}`);
 
-    } else if (realData.data[0]=="1"){
+    };*/
+    let realData = msgpack5().decode(x.slice(1, x.length));
+    if (realData.data[0]=="1"){
       console.log("user respawned");
        justDied = false;
+    } else if (realData.data[0]=="13"){
+        if (!IN_PROCESS && realData.data.length == 4 && realData.data[3]==0 &&realData.data[1]==0){
+            currentHat = realData.data[2];
+            console.log("Changed hat to " + currentHat);
+
+        }
+
     }
 };
 
@@ -91,6 +103,8 @@ function isElementVisible(e) {
 
 
 function heal(){
+    if (recentHealth>=100) return;
+    console.log(recentHealth);
     console.log(`HERE I AM IN THE HEAL FUNC with ${hasApple}`);
     var dataTemplate = {"data":[], "options":{"compress":false}, "nsp": "/", "type": 2};
     if (hasApple){
@@ -115,6 +129,8 @@ function heal(){
     let encoded = encodeSEND(dataTemplate);
     ws.send(encoded);
 
+    recentHealth += hasApple ? 20 : 40;
+
 }
 
 function handleMessage(m){
@@ -132,26 +148,36 @@ function handleMessage(m){
           console.log("doing stuff");
         console.log(info);
         if (info[2] < 100 && info[2] > 0){
+       recentHealth = info[2];
        console.log("RECEIVED:");
         console.log(info);
+        //recentHealth += hasApple ? 20 : 40;
        console.log("heal notif sent");
        setTimeout( () => {
            heal();
        }, autoHealSpeed);
         } else if (info[2] > 0) {
-            var dataTemplate = {"type": 2, "data":[], "options":{"compress":false}, "nsp": "/"};
-             dataTemplate["data"]=["4", 0, null];
-            let encoded = encodeSEND(dataTemplate);
-            ws.send(encoded);
+            console.log("done healing");
+            recentHealth = 100;
+            if (foodInHand){
+               console.log("okay bad thing happened");
+             var dataTemplate5 = {"type": 2, "data":[], "options":{"compress":false}, "nsp": "/"};
+             dataTemplate5["data"]=["5", 0, null];
+             let encoded5 = encodeSEND(dataTemplate5);
+             ws.send(encoded5);
+                console.log("corrected bad thing");
+            }
+
         } else {
             hasApple = true; //You've died tragically in combat; back to the apple for you!
-            console.log("Setting has apple to true from here")
+            console.log("Setting has apple to true from here");
         }
     }
     else if(info[0] == "11"){
         console.log("doing death");
         hasApple = true;
         justDied = true;
+        recentHealth = 100;
 
     }
 
