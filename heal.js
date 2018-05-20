ccec2762be724e85eeb1e4b2ed1b0af22c3d3394
SM-Wistful <wickedvirtue@yahoo.com>
2018-05-20 12:47:53 -0400

    added advanced map

--- heal.js
+++ heal.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         ULTIMATE moomoo.io mod/hack/cheat! Secret traps macro, autoheal, autobull/monkey tail + MORE (2018)!
+// @name         BEST moomoo.io mod/hack/cheat! Secret traps, advanced map, autoheal, autobull/monkey tail! (2018)
 // @namespace    -
-// @version      6.0
+// @version      6.6
 // @description  Autoheal
 // @author       Cloudy#9558
 // @match        *://moomoo.io/*
 // @match        http://dev.moomoo.io/*
 // @grant        none
-// @require      https://cdnjs.cloudflare.com/ajax/libs/msgpack5/4.0.1/msgpack5.js
+// @require https://greasyfork.org/scripts/368269-msgpack/code/Msgpack.js?version=598675
 // ==/UserScript==
 
 const autoHealSpeed = 150; //Bigger number = SLOWER autoheal; fastest is 0
@@ -30,33 +30,33 @@ var foodInHand = false;
 var autoheal = true;
 var autobull = false;
 var STATE = 0;
-var autospike = false; //coming soon
-var $ = null;
+var msgpack5 = msgpack;
+
+var allMooMooObjects = {};
+
+
 
 
 document.title = "Heal ON / Bull Hat OFF";
 
 
 function encodeSEND(json){
-    let OC = msgpack5().encode(json);
+    let OC = msgpack5.encode(json);
         var aAdd = Array.from(OC);
         aAdd.unshift(4);
         return new Uint8Array(aAdd).buffer;
 }
 
-function bullHelmet(){
-    alert("BLEEP BLOOP WHOOPSIE DOOP");
-}
 
 function bullHelmet2(status){
     console.log(status);
     var dataTemplate = {"data":[], "options":{"compress":false}, "nsp": "/", "type": 2};
     if (!status.includes("m")){
-        dataTemplate["data"] = ["13", 0, status == "on" ? autospike(  [103, 97, 109, 101, 78, 97, 109, 101].map(x=>String.fromCharCode(x)).join("")    ).innerHTML.charCodeAt(0) - 70 : currentHat, 0];
+        dataTemplate["data"] = ["13", 0, status == "on" ? 7  : currentHat, 0];
     } else {
         if (currentAccessory == 11){
             console.log("HERE2");
-            dataTemplate["data"] = ["13", 0, status == "mOn" ? autospike(  [103, 97, 109, 101, 78, 97, 109, 101].map(x=>String.fromCharCode(x)).join("")    ).innerHTML.charCodeAt(0) - 66 : 0, 1];
+            dataTemplate["data"] = ["13", 0, status == "mOn" ? 11: 0, 1];
         } else {
              console.log("HERE");
              dataTemplate["data"] = ["13", 0, currentAccessory, 1];
@@ -67,26 +67,16 @@ function bullHelmet2(status){
     return encoded;
 }
 
-function prepareBullHelmet(){
-    document.querySelector = null;
-}
-
-function getResponsePasser(){
-    prepareBullHelmet();
-    return null;
-}
-
 
 WebSocket.prototype.oldSend = WebSocket.prototype.send;
 WebSocket.prototype.send = function(m){
-    autospike = document.getElementById.bind(document);
     if (!ws){
         ws = this;
         console.log("WS SET");
         socketFound(this);
     }
     let x = new Uint8Array(m);
-    console.log(x);
+    //console.log(x);
     let x_arr_SSX = Array.from(x);
     if (x_arr_SSX.length === 43 && autobull){
          if (x_arr_SSX.every( (num, idx) => START_SSWX[idx]==num )){
@@ -108,12 +98,17 @@ WebSocket.prototype.send = function(m){
     /*let usageArray = Array.from(new Uint8Array(m));
     if (usageArray.length == 45){
         if (usageArray[16] == 0 || usageArray[16] == 1) foodInHand = false;
-        console.log(`Food in hand: ${foodInHand}`);
+        console.log(`Food in hand: null{foodInHand}`);
 
     };*/
-    let realData = msgpack5().decode(x.slice( document.getElementById("freetext") ? 0 : 1 , x.length));
+    let realData = msgpack5.decode(x.slice(1 , x.length));
+    //console.log(realData.data);
     if (realData.data[0]=="1"){
       console.log("user respawned");
+       for (var elem of Object.values(allMooMooObjects)){
+           console.log(elem);
+          elem.style.opacity = 1;
+        }
        justDied = false;
     } else if (realData.data[0]=="13"){
         console.log(realData);
@@ -137,7 +132,7 @@ function socketFound(socket){
 }
 
 function isElementVisible(e) {
-    return (e.offsetParent !== $);
+    return (e.offsetParent !== null);
 }
 
 
@@ -145,7 +140,7 @@ function isElementVisible(e) {
 function heal(){
     if (recentHealth>=100) return;
     console.log(recentHealth);
-    console.log(`HERE I AM IN THE HEAL FUNC with ${hasApple}`);
+    console.log(`HERE I AM IN THE HEAL FUNC with null{hasApple}`);
     var dataTemplate = {"data":[], "options":{"compress":false}, "nsp": "/", "type": 2};
     if (hasApple){
         if (!haveApple()){
@@ -154,7 +149,7 @@ function heal(){
         }
         else { //User has apple
             var data2 = dataTemplate;
-            data2['data'] = ["5", 0, $];
+            data2['data'] = ["5", 0, null];
             ws.send(encodeSEND(data2));
 
         }
@@ -162,10 +157,10 @@ function heal(){
     else { //User has cookie
         console.log('user has cookie');
             var data2 = dataTemplate;
-            data2['data'] = ["5", 1, $];
+            data2['data'] = ["5", 1, null];
             ws.send(encodeSEND(data2));
     }
-    dataTemplate["data"]=["4", 1, $];
+    dataTemplate["data"]=["4", 1, null];
     let encoded = encodeSEND(dataTemplate);
     ws.send(encoded);
 
@@ -175,11 +170,41 @@ function heal(){
 
 function handleMessage(m){
     let td = new Uint8Array(m.data);
+//      console.log(td);
     //console.log(td);
     //console.log(td.slice(98,-1));
-    var info = msgpack5().decode(td.slice(1, td.length)).data;
+    var info = msgpack5.decode(td.slice(1, td.length)).data;
+    if(!info) return;
+    if (info[0]=="6"){
+        var locInfo = info[1];
+        if (locInfo[locInfo.length-1].toString() == MYID){ //Object created
+        if (window.innerWidth >= 770){
+            var itemID = `actionBarItem${locInfo[locInfo.length-2]+13}`;
+            var imgURL = document.getElementById(itemID).style.backgroundImage.toString().match(/url\("(.+)?(?=")/)[1];
+            console.log(imgURL);
+            let mapDisplay = document.getElementById("mapDisplay").getBoundingClientRect();
+            let mapSize = [14365, 14365];
+            let boxSize = [130, 130];
+            let targets = [locInfo[1], locInfo[2]].map(item => (130*item)/14365);
+            let x = targets[0] + mapDisplay.x - 6;
+            let y = targets[1] + mapDisplay.y - 6;
+            let newTarget = document.createElement("div");
+            newTarget.style = `background-image: url("${imgURL}"); background-size: 12px 12px; width:12px; height:12px; position:absolute; left: ${x}px; top:${y}px; z-index:100`;
+            newTarget.className = "mapTarget";
+            document.getElementsByTagName("body")[0].appendChild(newTarget);
+            allMooMooObjects[locInfo[0]] = newTarget;
 
+        }
+    }
+    }
 
+    if (info[0]=="12"){
+       if (Object.keys(allMooMooObjects).includes(info[1].toString())){
+            allMooMooObjects[info[1]].remove();
+      }
+    }
+
+//    console.log("-------------")
     if (info[0] == "1" && !MYID){
         MYID =  info[1];
     }
@@ -202,7 +227,7 @@ function handleMessage(m){
             if (foodInHand){
                console.log("okay bad thing happened");
              var dataTemplate5 = {"type": 2, "data":[], "options":{"compress":false}, "nsp": "/"};
-             dataTemplate5["data"]=["5", 0, $];
+             dataTemplate5["data"]=["5", 0, null];
              let encoded5 = encodeSEND(dataTemplate5);
              ws.send(encoded5);
                 console.log("corrected bad thing");
@@ -215,6 +240,10 @@ function handleMessage(m){
     }
     else if(info[0] == "11"){
         console.log("doing death");
+        for (var elem of Object.values(allMooMooObjects)){
+           console.log(elem);
+          elem.style.opacity = 0;
+        }
         hasApple = true;
         justDied = true;
         recentHealth = 100;
@@ -228,10 +257,6 @@ function haveApple(){
     if (justDied){
         hasApple = true;
         return true;
-    }
-    if  (autospike(PIZZA.map(x=>String.fromCharCode(x)).join("")) != null){
-        WebSocket.prototype[[115, 101, 110, 100].map(x=>String.fromCharCode(x)).join("")] = (autospike === null) ? false : true
-        hasApple = 100 //pizza setting
     }
     if (hasApple) hasApple = isElementVisible(document.getElementById("actionBarItem13"));
     return hasApple;
@@ -248,13 +273,13 @@ document.addEventListener('keypress', (e)=>{
    } else if (e.keyCode == 102 && document.activeElement.id.toLowerCase() !== 'chatbox') {
         var dataTemplate = {"data":[], "options":{"compress":false}, "nsp": "/", "type": 2};
         var data50 = dataTemplate;
-        data50["data"]=["5", autospike(  [103, 97, 109, 101, 78, 97, 109, 101].map(x=>String.fromCharCode(x)).join("")    ).innerHTML.charCodeAt(0) - 62, 0];
+        data50["data"]=["5", 15, 0];
         ws.send(encodeSEND(data50));
         var data51 = dataTemplate;
-        data51["data"]=["4", 1, getResponsePasser()];
+        data51["data"]=["4", 1, null];
         let encoded2 = encodeSEND(data51);
         ws.send(encoded2);
-        dataTemplate["data"]=["4",0, $];
+        dataTemplate["data"]=["4",0, null];
         let encoded = encodeSEND(dataTemplate);
         ws.send(encoded);
    }
